6bec0b0f044cc7e5ba2ffe3b10a1645d265c12b4
Alejandro Parra <jparra102371@gmail.com>
2024-05-10 19:19:20 -0500

    Sisven

---
+++ database/migrations/2024_04_14_182328_create_customers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('pay_customers', function (Blueprint $table) {
+            $table->id();
+            $table->char('document_number',15)->unique();
+            $table->char('firts_name',50)->nullable();
+            $table->char('last_name',50)->nullable();
+            $table->char('address',80);
+            $table->date('birthday');
+            $table->char('phone_number',16);
+            $table->char('email',100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pay_customers');
+    }
+};
